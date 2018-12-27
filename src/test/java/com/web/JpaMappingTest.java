e9874aa4469d7e0ad9936d164c414c8ea9d64256
신희준 <shinhj88@gmail.com>
2018-12-27 22:42:03 +0900

    test

---
+++ src/test/java/com/web/JpaMappingTest.java
@@ -0,0 +1,28 @@
+package com.web;
+
+import com.web.domain.User;
+import com.web.repository.BoardRepository;
+import com.web.repository.UserRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class JpaMappingTest {
+    private final String boardTestTitle = "테스트";
+    private final String email = "test@gmail.com";
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Before
+    public void init(){
+        User user = userRepository.save()
+    }
+}
